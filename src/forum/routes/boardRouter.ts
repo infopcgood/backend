1d7b2311a8e554affe2214681f0de3978a57f009
Jaemin Shim <sshs_student_412@outlook.kr>
2023-07-19 09:43:05 +0900

    make browser by board function

--- src/forum/routes/boardRouter.ts
+++ src/forum/routes/boardRouter.ts
@@ -1,5 +1,8 @@
 import { Context, Next } from "koa"
 import Router from "koa-router"
+import { ArticleModel } from "../models/articleModels"
+import { validateToken } from "../../functions/tokenValidation"
+import { AnonArticleModel } from "../models/anonArticleModels"
 
 const availableBoards = ['main','study','gaming','future']
 const availableAnonBoards = ['anon','forest']
@@ -10,5 +13,18 @@ type State = {
 const boardRouter = new Router<State>()
 
 boardRouter.get('/api/board:id', async (ctx: Context, next: Next) => {
-    ctx.params.
+    await validateToken(ctx)
+    const boardName = ctx.params.id
+    if (availableBoards.find(boardName) !== undefined) {
+        ctx.status = 200
+        ctx.body = await ArticleModel.find({ board: boardName },{ projection: {__v: 0, _id: 0, content: 0} }).sort('-time_written')
+    }
+    else if (availableAnonBoards.find(boardName) !== undefined) {
+        ctx.status = 200
+        ctx.body = await AnonArticleModel.find({ board: boardName },{ projection: {__v: 0, _id: 0, content: 0, password_sha256: 0} }).sort('-time_written')
+    }
+    else {
+        ctx.throw(404)
+    }
+    await next()
 })
